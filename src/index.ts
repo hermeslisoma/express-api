f945cbb4030834d2847192e99dc2c676fc444e41
Hermes <hermeslisoma@gmail.com>
2019-06-02 04:23:42 -0400

    test pipeline

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ app.use(cors({
     origin:'http://localhost:3000',
     credentials: true
 }))
+
 //Endpoint to register a new user with
 //prior validation middleware
 app.post('/register', validationPostUser(), async (req, res) =>{
