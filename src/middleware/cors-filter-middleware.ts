8c1590beb1b233c381bd4a7a8d309655cdc3b299
Hermes <hermeslisoma@gmail.com>
2019-06-10 01:10:32 -0400

    remove old cors

--- src/middleware/cors-filter-middleware.ts
+++ src/middleware/cors-filter-middleware.ts
@@ -1,10 +1,10 @@
-import { Request, Response} from "express"
+// import { Request, Response} from "express"
 
-export function corsFilter (req:Request, res:Response, next){
-    console.log(req.headers);
+// export function corsFilter (req:Request, res:Response, next){
+//     console.log(req.headers);
     
-    res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)
-    res.header('Access-Control-Allow-Headers','Origin, X-requested-With,Content-type, Accept')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    next()
-}
+//     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)
+//     res.header('Access-Control-Allow-Headers','Origin, X-requested-With,Content-type, Accept')
+//     res.header('Access-Control-Allow-Credentials', 'true')
+//     next()
+// }
