73032a58962af872a0c607799654caf9e16e3792
Hermes <hermeslisoma@gmail.com>
2019-06-10 00:01:38 -0400

    add db info

--- src/dao/index.ts
+++ src/dao/index.ts
@@ -2,10 +2,10 @@ import {Pool} from 'pg'
 
 //Connection factory
 export const connectionPool:Pool = new Pool({
-    user: process.env['ERS_DB_USER'],
-    host: process.env['ERS_DB_HOST'],
-    database: process.env['ERS_DB_NAME'],
-    password: process.env['ERS_DB_PASS'],
+    user: 'hermeslisoma',
+    host: 'hermeslisoma.cvnmtqur08tc.us-east-2.rds.amazonaws.com',
+    database: 'hermeslisoma',
+    password: 'hermes243',
     port: 5432,
     max: 5
 })
