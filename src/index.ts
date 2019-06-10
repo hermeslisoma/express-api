73e7aef26cf64fa096c429416f337796b9ffe57b
Hermes <hermeslisoma@gmail.com>
2019-06-09 21:18:35 -0400

    all origin

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors({
     //origin: 'http://localhost:3000',
-    origin: 'http://hermes-react.s3-website.us-east-2.amazonaws.com',
+    //origin: 'http://hermes-react.s3-website.us-east-2.amazonaws.com',
+    origin: '*',
     credentials: true
     //methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
 }))
